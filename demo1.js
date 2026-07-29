50df5d8d3729c420d2a8e4c6a7ae975cd5ea63f1
aayushi1403 <aayushir685@gmail.com>
2026-07-29 23:47:41 +0530

    objects demo

---
+++ demo1.js
@@ -0,0 +1,5 @@
+let a=10;
+// let a=20; //gives error
+a=90;
+console.log(a)
+
---
+++ demo2.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "aayushi",
+  age: 20,
+  branch: "cse"
+};
+
+const { name, age, branch } = student;
+
+console.log(name);    // aayushi
+console.log(age);     // 20
+console.log(branch);  // cse
