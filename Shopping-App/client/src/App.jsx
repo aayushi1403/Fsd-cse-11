1ec6fbb29ecd6d9f9f2126e74adab8b208f91100
aayushi1403 <aayushir685@gmail.com>
2026-09-11 09:27:08 +0530

    react routes used

--- Shopping-App/client/src/App.jsx
+++ Shopping-App/client/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './App.css'
 import UserLayout from './Pages/UserLayout'
 function App() {
   return (
-    <>
-      <UserLayout/>
-    </>
+    
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<UserLayout/>}/>
+    <Route path="/mycart" element={<h1>My cart</h1>}/>
+    <Route path="/myorders" element={<h1>My Orders</h1>}/>
+    <Route path="/settings" element={<h1>Settings Page</h1>}/>
+    <Route path="/profile" element={<h1>My Profile</h1>}/>
+    <Route path="/logout" element={<h1>Logout</h1>}/>
+    <Route path="/*" element={<h1>Error Page</h1>}/>
+      </Routes>
+    </BrowserRouter>
+   
+    
+    
   )
 }
 
--- Shopping-App/client/src/Components/Navbar.jsx
+++ Shopping-App/client/src/Components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='navbar'>
 
-      <a>Home</a>
-      <a>Mycart</a>
-      <a>Myorders</a>
-      <a>settings</a>
-      <a>My profile</a>
-      <a>Logout</a>
+      <Link to="/">Home</Link>
+      <Link to="/mycart">Mycart</Link>
+      <Link to="/myorders">Myorders</Link>
+      <Link to="/settings">settings</Link>
+      <Link to="/profile">My profile</Link>
+      <Link to="logout/">Logout</Link>
 
     </div>
   )
